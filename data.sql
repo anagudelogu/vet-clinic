cd9c029f0b4f7f17b27690e2e67ae7443d3d4006
Andres Agudelo <anagudelogu@gmail.com>
2022-07-05 15:58:41 -0500

    Populate animals table with data

--- data.sql
+++ data.sql
@@ -1,5 +1,12 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Agumon', '2020-02-3', 0, B '1', 10.23),
+  ('Gabumon', '2018-11-15', 2, B '1', 8.0),
+  ('Pikachu', '2021-01-07', 1, B '0', 15.04),
+  ('Devimon', '2017-05-12', 5, B '1', 11.0);
