fc78e9d9d95a9848dddd5a5101ee7e14930d1616
Andres Agudelo <anagudelogu@gmail.com>
2022-07-05 21:30:41 -0500

    Add required queries and answers to analytical questions

--- queries.sql
+++ queries.sql
@@ -34,3 +34,29 @@ WHERE NOT name = 'Gabumon';
 SELECT *
 FROM animals
 WHERE weight_kg BETWEEN 10.4 and 17.3;
+-- How many animals are there?
+SELECT COUNT(*)
+FROM animals;
+-- How many animals have never tried to escape?
+SELECT COUNT(escape_attempts)
+FROM animals
+WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg)
+FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT AVG(escape_attempts)
+FROM animals
+GROUP BY neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species,
+  MIN(weight_kg) as MIN_WEIGHT,
+  MAX(weight_kg) as MAX_WEIGHT
+FROM animals
+GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species,
+  AVG(escape_attempts) as Average_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
